7e216a1c00a00ec2a6964696a4cabbdfeb979c1d
Conor O Reilly <conororeilly@Conors-MacBook-Pro.local>
2020-03-17 13:09:42 +0000

    added roll function and DOM manipulation

--- app.js
+++ app.js
@@ -8,3 +8,51 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+var scores, roundScore, activePlayer, dice;
+
+scores = [0,0];
+roundScore = 0;
+activePlayer = 1;
+
+dice = Math.floor(Math.random() * 6) + 1;
+
+document.querySelector('#current-' + activePlayer).textContent = dice; // Maniplulate to change values on webpage
+// document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
+
+var x = document.querySelector('#score-0').textContent; // Can read from the webpage
+console.log(x);
+
+document.querySelector('.dice').style.display = 'none'; // Changs CSS - hide dice
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
