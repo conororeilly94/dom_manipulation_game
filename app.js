4f765ad4a7154c99ad05932d6b3cbbb5f11ca901
Conor O Reilly <conororeilly@Conors-MacBook-Pro.local>
2020-03-17 13:28:40 +0000

    added dice image and new score values

--- app.js
+++ app.js
@@ -9,22 +9,32 @@ GAME RULES:
 
 */
 
-var scores, roundScore, activePlayer, dice;
+var scores, roundScore, activePlayer;
 
 scores = [0,0];
 roundScore = 0;
 activePlayer = 1;
 
-dice = Math.floor(Math.random() * 6) + 1;
-
-document.querySelector('#current-' + activePlayer).textContent = dice; // Maniplulate to change values on webpage
-// document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
-
-var x = document.querySelector('#score-0').textContent; // Can read from the webpage
-console.log(x);
-
 document.querySelector('.dice').style.display = 'none'; // Changs CSS - hide dice
 
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+    
+    // 1. Random number 
+    var dice = Math.floor(Math.random() * 6) + 1;
+    
+    // 2. Display result
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+    
+    // 3. Update the round score IF the rolled number was NOT a 1
+    
+});
 
 
 
@@ -48,8 +58,10 @@ document.querySelector('.dice').style.display = 'none'; // Changs CSS - hide dic
 
 
 
+// document.querySelector('#current-' + activePlayer).textContent = dice; // Maniplulate to change values on webpage
+// document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>';
 
-
+// var x = document.querySelector('#score-0').textContent; // Can read from the webpage
 
 
 
