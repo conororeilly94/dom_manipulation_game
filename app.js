3bf88e8c03921bb7284976bf3d8906fdf91141e6
Conor O Reilly <conororeilly@Conors-MacBook-Pro.local>
2020-03-17 16:02:58 +0000

    added score totals and announced winner

--- app.js
+++ app.js
@@ -39,7 +39,31 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
         document.querySelector('#current-' + activePlayer).textContent = roundScore; // Maniplulate to change values on webpage
     } else {
         // Next player
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        nextPlayer();
+    }
+});
+
+document.querySelector('.btn-hold').addEventListener('click', function() {
+    // Add Current score to Global score
+    scores[activePlayer] += roundScore;
+    
+    // Update the GUI
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+    
+    // Check if player won the game
+    if (scores[activePlayer] >= 10) {
+        document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
+        document.querySelector('.dice').style.display = 'none';
+        document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+        document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+    } else {
+        // Next player
+        nextPlayer();
+    }
+});
+
+function nextPlayer() {
+     activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
         roundScore = 0;
         
         document.getElementById('current-0').textContent = '0';
@@ -52,12 +76,7 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
         // document.querySelector('.player-1-panel').classList.add('active');
         
         document.querySelector('.dice').style.display = 'none';
-    }
-});
-
-
-
-
+}
 
 
 
